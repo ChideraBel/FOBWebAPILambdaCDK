99b41ca4b9502438efc9900b2e231a4e2276443e
chi-ops <cbeluonwu@hawk.iit.edu>
2024-02-05 17:12:11 -0600

    first commit, add lambda function target file to deploy

--- lib/fob_web_api_lambda_cdk-stack.ts
+++ lib/fob_web_api_lambda_cdk-stack.ts
@@ -1,5 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class FobWebApiLambdaCdkStack extends cdk.Stack {
@@ -12,5 +15,44 @@ export class FobWebApiLambdaCdkStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'FobWebApiLambdaCdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // Create an IAM role
+    const iamRole = new iam.Role(this, 'MyLambdaRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    });
+
+    // Define an inline policy statement for DynamoDB access with specific actions and resource ARNs
+    const dynamoDBPolicyStatement1 = new iam.PolicyStatement({
+      actions: ['dynamodb:Scan', 'dynamodb:Query'],
+      resources: ['arn:aws:dynamodb:*:730335448770:table/*/index/*'],
+    });
+
+    const dynamoDBPolicyStatement2 = new iam.PolicyStatement({
+      actions: [
+        'dynamodb:BatchGetItem',
+        'dynamodb:BatchWriteItem',
+        'dynamodb:PutItem',
+        'dynamodb:DeleteItem',
+        'dynamodb:GetItem',
+        'dynamodb:Scan',
+        'dynamodb:Query',
+        'dynamodb:UpdateItem',
+      ],
+      resources: ['arn:aws:dynamodb:*:730335448770:table/*'],
+    });
+
+    // Attach the inline policy to the IAM role
+    iamRole.addToPolicy(dynamoDBPolicyStatement1);
+    iamRole.addToPolicy(dynamoDBPolicyStatement2);
+
+    const mainLambda = new lambda.Function(this, 'FOBWebAPILambda-Function', {
+      runtime: lambda.Runtime.JAVA_11,
+      handler: 'org.tranqwave.fobwebapilambda.MainLambda::handleRequest', // Replace with your handler class and method
+      code: lambda.Code.fromAsset('../FOBWebAPILambda/target/FOBWebAPILambda-1.0-SNAPSHOT.jar'), // Use relative path to Java code directory
+      role: iamRole,
+      memorySize: 500,
+      timeout: cdk.Duration.seconds(10)
+    });
+    
   }
 }
